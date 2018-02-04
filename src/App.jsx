de60401dd990913ce239940c8303264aa753edec
Hans Burbano <nslaver@gmail.com>
2018-02-04 18:48:10 -0500

    Add base theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { ThemeProvider } from 'styled-components'
+
+const theme = {
+  main: 'mediumseagreen'
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </ThemeProvider>
+    )
   }
 }
 
-export default App;
+export default App
