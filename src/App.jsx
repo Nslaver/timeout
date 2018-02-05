aa72b49929ad768194826817430a9eaeed868edf
Hans Burbano <nslaver@gmail.com>
2018-02-04 20:56:23 -0500

    Use base Home componet in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import React, { Component } from 'react'
 import { ThemeProvider } from 'styled-components'
-
-const theme = {
-  main: 'mediumseagreen'
-}
+import Home from 'components/Home'
+import theme from 'shared/theme'
 
 class App extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <div className="App">
-          <header className="App-header">
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
+        <Home />
       </ThemeProvider>
     )
   }
